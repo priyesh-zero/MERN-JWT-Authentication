21c22c204c622039cd13e64aa0b1476ca69c351d
priyesh-zero <priyesh.zero@gmail.com>
2019-11-26 14:33:20 +0530

    Login Functionality implemented with the Json Placeholder

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,33 @@
-var router = require('express').Router()
+const router = require('express').Router()
+
+const axios = require('../axios/axios')
+const { sign } = require('../jwt/jwt-module')
 
 router.post('/login', (req, res) => {
-    res.json(req.body)
+    axios
+        .get(`users?email=${req.body.email}&username=${req.body.password}`)
+        .then(response => {
+            if (response.data.length > 0) { 
+                res.json({
+                    success: true,
+                    code: 200,
+                    token: sign(response.data[0].id)
+                })
+             } else {
+                res.json({
+                    success: false,
+                    code: 401,
+                    error: 'Invalid Credentials'
+                })
+             }
+        })
+        .catch(err => {
+            res.json({
+                success: false,
+                code: 500,
+                error: 'Error with the Database'
+            })
+        })
 })
 
 module.exports = router
